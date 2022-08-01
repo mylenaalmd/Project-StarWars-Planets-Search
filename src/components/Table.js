8664208fe1da49675b451e5a28994e776aed8530
Mylena Nunes <mylenanunesdealmeida112@gmail.com>
2022-07-31 23:14:24 -0300

    NEW: implementação por partes; Primeira parte adição dos filtros por nome, números, multiplos filtros e por ordem

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,31 +1,165 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import context from '../context/context';
 
 function Table() {
-  const { data } = useContext(context);
+  const { data, addFiltros,
+    filters, nameFilter, sorte,
+  } = useContext(context);
+  const [name, setName] = useState('');
+  const [comparison, setComparison] = useState('maior que');
+  const [valueNumber, setValueNumber] = useState(0);
+  const [column, setColumn] = useState('population');
+  const [sort, setSort] = useState('ASC');
+  const [sortC, setSortC] = useState('population');
+  const filter = {
+    comparison,
+    column,
+    valueNumber,
+  };
+  const order = {
+    sort,
+    sortC,
+  };
+
+  const keyCheck = (key) => {
+    if (key === 'name') {
+      return 'planet-name';
+    }
+    return (key);
+  };
+
+  const arrayFiltros = [
+    'population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
+  const util = filters.map((e) => e.column);
+  const disponible = arrayFiltros.filter((e) => !util.includes(e));
 
   return (
-    <table>
-      <tr>
+    <div>
+      <input
+        type="text"
+        data-testid="name-filter"
+        value={ name }
+        onChange={ ({ target: { value } }) => {
+          setName(value);
+          nameFilter(value);
+        } }
+      />
+
+      {filters.length < arrayFiltros.length ? (
+        <div>
+          <select
+            value={ column }
+            data-testid="column-filter"
+            onChange={ ({ target: { value } }) => setColumn(value) }
+          >
+            {
+              disponible.map(
+                (filtro, i) => (
+                  <option
+                    key={ i }
+                    value={ filtro }
+                  >
+                    {filtro}
+                  </option>
+                ),
+              )
+            }
+          </select>
+          <select
+            value={ comparison }
+            onChange={ ({ target: { value } }) => setComparison(value) }
+            data-testid="comparison-filter"
+          >
+            <option value="maior que">
+              maior que
+            </option>
+            <option value="menor que">
+              menor que
+            </option>
+            <option value="igual a">
+              igual a
+            </option>
+          </select>
+          <input
+            type="number"
+            data-testid="value-filter"
+            value={ valueNumber }
+            onChange={ ({ target: { value } }) => setValueNumber(value) }
+          />
+          <button
+            type="button"
+            data-testid="button-filter"
+            onClick={ () => { addFiltros(filter); setColumn('population'); } }
+          >
+            adicionar filtros
+          </button>
+          <select
+            value={ sortC }
+            data-testid="column-sort"
+            onChange={ ({ target: { value } }) => setSortC(value) }
+          >
+            {arrayFiltros.map((f, index) => (
+              <option
+                key={ index }
+                value={ f }
+              >
+                {f}
+              </option>
+            ))}
+          </select>
+          <label htmlFor="asc">
+            ascendente
+            <input
+              type="radio"
+              data-testid="column-sort-input-asc"
+              value="ASC"
+              name="sort"
+              id="asc"
+              onClick={ ({ target }) => setSort(target.value) }
+            />
+          </label>
+          <label htmlFor="desc">
+            descendente
+            <input
+              type="radio"
+              data-testid="column-sort-input-desc"
+              value="DESC"
+              name="sort"
+              id="desc"
+              onClick={ ({ target }) => setSort(target.value) }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="column-sort-button"
+            onClick={ () => sorte(order) }
+          >
+            ordenar por
+          </button>
+        </div>
+      ) : false}
+
+      <table>
+        <tr>
+          {
+            data ? (
+              Object.keys(data[0]).map((item, index) => <th key={ index }>{item}</th>)
+            ) : false
+          }
+        </tr>
         {
           data ? (
-            Object.keys(data[0]).map((item, index) => <th key={ index }>{item}</th>)
+            data.map((planet) => (
+              <tr key={ planet.url }>
+                { Object.keys(planet).map((key) => (
+                  <td key={ key } data-testid={ keyCheck(key) }>{planet[key]}</td>
+                ))}
+              </tr>
+            ))
           ) : false
         }
-      </tr>
-      {
-        data ? (
-          data.map((planet) => (
-            <tr key={ planet.url }>
-              { Object.keys(planet).map((key) => (
-                <td key={ key }>{planet[key]}</td>
-              ))}
-
-            </tr>
-          ))
-        ) : false
-      }
-    </table>
+      </table>
+    </div>
   );
 }
 
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -4,30 +4,88 @@ import Context from './context';
 
 function Provider({ children }) {
   const [data, setData] = useState();
+  const [filters, setFilter] = useState([]);
+
+  const fetchApiPlanets = async () => {
+    const dataPlanet = await fetch('https://swapi-trybe.herokuapp.com/api/planets/')
+      .then((response) => response.json())
+      .then((item) => item.results);
+    dataPlanet.forEach((planet) => delete planet.residents);
+    const planetsSorted = dataPlanet.sort((a, b) => {
+      if (a.name < b.name) {
+        const NUMBER = -1;
+        return NUMBER;
+      }
+      return true;
+    });
+    setData(planetsSorted);
+    console.log(planetsSorted);
+  };
 
   useEffect(() => {
-    const fetchApiPlanets = async () => {
-      const dataPlanet = await fetch('https://swapi-trybe.herokuapp.com/api/planets/')
-        .then((response) => response.json())
-        .then((item) => item.results);
-      dataPlanet.forEach((planet) => delete planet.residents);
-      const planeteSorted = dataPlanet.sort((a, b) => {
-        if (a.name < b.name) {
-          const NUMBER = -1;
-          return NUMBER;
-        }
-        return true;
-      });
-      setData(planeteSorted);
-      console.log(planeteSorted);
-    };
     fetchApiPlanets();
   }, []);
+
+  const sorte = ({ sort, sortC }) => {
+    const planets = data.filter((planet) => planet[sortC] !== 'unknown');
+    const planetsDefault = data.filter((planet) => planet[sortC] === 'unknown');
+
+    if (sort === 'ASC') {
+      const planetsSorted = planets.sort((a, b) => a[sortC] - b[sortC]);
+      const dataSorted = [...planetsSorted, ...planetsDefault];
+      setData(dataSorted);
+    } else {
+      const planetsSorted = planets.sort((a, b) => b[sortC] - a[sortC]);
+      const dataSorted = [...planetsSorted, ...planetsDefault];
+      setData(dataSorted);
+    }
+  };
+
+  const nameFilter = (value) => {
+    const planets = data.filter((planet) => planet.name.includes(value));
+    if (planets.length > 0) { setData(planets); }
+    if (value.length < 1) { fetchApiPlanets(); }
+  };
+
+  const addFiltros = (addFilter) => {
+    setFilter([...filters, addFilter]);
+    setData(data.filter((item) => item[addFilter.column] !== 'unknown'));
+    switch (addFilter.comparison) {
+    case 'maior que':
+      setData(
+        data.filter((item) => parseInt(
+          item[addFilter.column], 10,
+        ) > addFilter.valueNumber),
+      );
+      break;
+    case 'menor que':
+      setData(
+        data.filter((item) => parseInt(
+          item[addFilter.column], 10,
+        ) < addFilter.valueNumber),
+      );
+      break;
+    default:
+      setData(
+        data.filter(
+          (item) => parseInt(
+            item[addFilter.column], 10,
+          ) === parseInt(addFilter.valueNumber, 10),
+        ),
+      );
+    }
+  };
 
   return (
     <Context.Provider
       value={ {
         data,
+        filters,
+        setFilter,
+        fetchApiPlanets,
+        nameFilter,
+        addFiltros,
+        sorte,
       } }
     >
       {children}
